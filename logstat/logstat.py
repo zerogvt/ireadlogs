f89227338a8317571e1f5a097be8b82246e77dc6
zerogvt <gkoumplias@gmail.com>
2022-01-16 23:36:40 +0200

    main logic

--- logstat/logstat.py
+++ logstat/logstat.py
@@ -6,6 +6,7 @@ import argparse
 from fileinput import lineno
 import sys
 import re
+import json
 
 def get_parser():
     ''' Parses command line arguments '''
@@ -18,14 +19,20 @@ def get_parser():
 
 
 def read_log():
-    p = re.compile(r'(.*)\s-\s-\s\[(.*)\]\s"(\w+)\s(.*)\s(.*)"\s(\d+)\s(\d+)?')
+    res = {
+        "top_10_req_pages": [],
+        "perc_succ_reqs": 0,
+        "perc_fail_reqs": 0,
+        "top_10_hosts": [],
+        "error_in_lines": None
+    }
+    p = re.compile(r'(.*)\s-\s-\s\[(.*)\]\s"(\w+)\s(.*)(\s.*)?"\s(\d+)\s(\d+)?')
     lineno = 0
     hosts = {}
+    pages = {}
     errors = []
-    succ = 0
-    fails = 0
     lineno = 0
-    with open("NASA_access_log_Aug95", "r") as logfile:
+    with open("NASA_access_log_Aug95", "r", errors='replace') as logfile:
         for line in logfile:
             lineno += 1
             try:
@@ -33,28 +40,53 @@ def read_log():
                 # unpack parsed values
                 nbytes = 0
                 if len(m.groups()) == 7:
-                    host, t, verb, img, vers, status, nbytes = m.groups()
+                    host, t, verb, page, vers, status, nbytes = m.groups()
                 elif len(m.groups()) == 6:
-                    host, t, verb, img, vers, status = m.groups()
+                    host, t, verb, page, vers, status = m.groups()
                 else:
                     errors.append(lineno)
                     continue
-                if int(status) >= 400 or int(status) < 200:
-                    fails += 1
+                if page not in pages:
+                    pages[page] = { 'count': 1, 'fails': 0}
                 else:
-                    succ += 1
+                    pages[page]['count'] += 1
+
                 if host not in hosts:
-                    hosts[host] = 1
+                    hosts[host] = { 'count': 1, 'fails': 0, 'pages': { page: 1 }}
                 else:
-                    hosts[host] += 1
+                    hosts[host]['count'] += 1
+                    if page not in hosts[host]['pages']:
+                        hosts[host]['pages'][page] = 1
+                    else:
+                        hosts[host]['pages'][page] += 1
+                if int(status) >= 400 or int(status) < 200:
+                    hosts[host]['fails'] += 1
+                    pages[page]['fails'] += 1
             except (UnicodeDecodeError, AttributeError):
                 errors.append(lineno)
                 continue
-    hosts_freq = sorted(hosts.keys(), key=lambda x: hosts[x], reverse=True)
-    print(succ, fails)
-    #for h in hosts_freq[:20]:
-    #    print(h, hosts[h])
+    pages_freq = sorted(pages.keys(), key=lambda x: pages[x]['count'], reverse=True)
+    for p in pages_freq[:10]:
+        res['top_10_req_pages'].append((p, pages[p]['count']))
+    
+    fails, total = 0, 0
+    for h in hosts:
+        fails += hosts[h]['fails']
+        total += hosts[h]['count']
+    print(fails, total)
+    succ = total - fails
+    res['perc_succ_reqs'] = f"{100 * succ/total:.2f}"
+    res['perc_fail_reqs'] = f"{100 * fails/total:.2f}"
 
+    hosts_freq = sorted(hosts.keys(), key=lambda x: hosts[x]['count'], reverse=True)
+    for h in hosts_freq[:10]:
+        res['top_10_hosts'].append({ 'host': h , 'count': hosts[h]['count'], 'breakdown': []})
+        pages_freq_for_host = sorted(hosts[h]['pages'].keys(), key=lambda x: hosts[h]['pages'][x], reverse=True)
+        for p in pages_freq_for_host[:5]:
+            res['top_10_hosts'][-1]['breakdown'].append((p, hosts[h]['pages'][p]))
+
+    res['error_in_lines'] = errors
+    return res
 
 
 def execute_script(input_args):
@@ -62,7 +94,8 @@ def execute_script(input_args):
     print(parsed_args)
     if parsed_args.test:
         print("Testing the app.")
-    read_log()
+    res = read_log()
+    print(json.dumps(res, indent=4))
 
 
 def main():
