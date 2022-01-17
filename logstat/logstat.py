9f4091348205989d74d7aaa3dedb1931ea00987e
zerogvt <gkoumplias@gmail.com>
2022-01-17 18:23:06 +0200

    configuration

--- logstat/logstat.py
+++ logstat/logstat.py
@@ -1,43 +1,52 @@
-'''
-This is a script that will be used in the setup.py as an entry point.
-Instantiate all your classes here.
-'''
-import argparse
-from fileinput import lineno
 import sys
 import re
 import json
 
-def get_parser():
-    ''' Parses command line arguments '''
 
-    parser = argparse.ArgumentParser(description=
-            "Description of the app that will be displayed when the script is executed.")
-    parser.add_argument('--test', help="Test the app.", dest="test",
-                        action='store_true', required=False)
-    return parser
+class Config:
+    """captures run configuration"""
+
+    def __init__(self, cfgdict):
+        self.logfile = cfgdict["logfile"]
+        self.top_req_pages = cfgdict["top_req_pages"]
+        self.perc_succ_reqs = cfgdict["perc_succ_reqs"]
+        self.perc_fail_reqs = cfgdict["perc_fail_reqs"]
+        self.top_hosts = cfgdict["top_hosts"]
+        self.top_pages_per_host = cfgdict["top_pages_per_host"]
+        self.error_in_lines = cfgdict["error_in_lines"]
 
 
-def read_log():
+def stats(config):
+    """
+    Read log file and keep stats as you go along.
+    This is linear and not multicore.
+    We can follow a map-reduce approach if we break up the log file
+    (more on the second part of the take home assignment).
+    """
     res = {
-        "top_10_req_pages": [],
+        "top_req_pages": [],
         "perc_succ_reqs": 0,
         "perc_fail_reqs": 0,
-        "top_10_hosts": [],
-        "error_in_lines": None
+        "top_hosts": [],
+        "error_in_lines": None,
     }
+    # reg exp that parses general log format. Some groups are optional (?)
     p = re.compile(r'(.*)\s-\s-\s\[(.*)\]\s"(\w+)\s(.*)(\s.*)?"\s(\d+)\s(\d+)?')
     lineno = 0
+    # that's the basic stats index based on hosts
     hosts = {}
+    # secondary index to help with pages stats
     pages = {}
+    # another index to keep error lines
     errors = []
     lineno = 0
-    with open("NASA_access_log_Aug95", "r", errors='replace') as logfile:
-        for line in logfile:
+    with open(config.logfile, "r", errors="replace") as logf:
+        # Can't do better than O(n) - we need to read through all log file
+        for line in logf:
             lineno += 1
             try:
                 m = p.match(line)
-                # unpack parsed values
+                # unpack parsed values - nbytes is optional
                 nbytes = 0
                 if len(m.groups()) == 7:
                     host, t, verb, page, vers, status, nbytes = m.groups()
@@ -46,61 +55,66 @@ def read_log():
                 else:
                     errors.append(lineno)
                     continue
+                # keep pages-based stats
                 if page not in pages:
-                    pages[page] = { 'count': 1, 'fails': 0}
+                    pages[page] = {"count": 1, "fails": 0}
                 else:
-                    pages[page]['count'] += 1
-
+                    pages[page]["count"] += 1
+                # keep hosts-based stats...
                 if host not in hosts:
-                    hosts[host] = { 'count': 1, 'fails': 0, 'pages': { page: 1 }}
+                    hosts[host] = {"count": 1, "fails": 0, "pages": {page: 1}}
                 else:
-                    hosts[host]['count'] += 1
-                    if page not in hosts[host]['pages']:
-                        hosts[host]['pages'][page] = 1
+                    hosts[host]["count"] += 1
+                    # ...keep stats that relate hosts to pages
+                    if page not in hosts[host]["pages"]:
+                        hosts[host]["pages"][page] = 1
                     else:
-                        hosts[host]['pages'][page] += 1
+                        hosts[host]["pages"][page] += 1
+                # ...and stats relating hosts to http errors
                 if int(status) >= 400 or int(status) < 200:
-                    hosts[host]['fails'] += 1
-                    pages[page]['fails'] += 1
+                    hosts[host]["fails"] += 1
+                    pages[page]["fails"] += 1
+            # any error contributes to errors index
             except (UnicodeDecodeError, AttributeError):
                 errors.append(lineno)
                 continue
-    pages_freq = sorted(pages.keys(), key=lambda x: pages[x]['count'], reverse=True)
-    for p in pages_freq[:10]:
-        res['top_10_req_pages'].append((p, pages[p]['count']))
-    
+    # extract stats for pages
+    pages_freq = sorted(pages.keys(), key=lambda x: pages[x]["count"], reverse=True)
+    for p in pages_freq[: config.top_req_pages]:
+        res["top_req_pages"].append((p, pages[p]["count"]))
+    # extract success/failure rates
     fails, total = 0, 0
     for h in hosts:
-        fails += hosts[h]['fails']
-        total += hosts[h]['count']
-    print(fails, total)
+        fails += hosts[h]["fails"]
+        total += hosts[h]["count"]
     succ = total - fails
-    res['perc_succ_reqs'] = f"{100 * succ/total:.2f}"
-    res['perc_fail_reqs'] = f"{100 * fails/total:.2f}"
-
-    hosts_freq = sorted(hosts.keys(), key=lambda x: hosts[x]['count'], reverse=True)
-    for h in hosts_freq[:10]:
-        res['top_10_hosts'].append({ 'host': h , 'count': hosts[h]['count'], 'breakdown': []})
-        pages_freq_for_host = sorted(hosts[h]['pages'].keys(), key=lambda x: hosts[h]['pages'][x], reverse=True)
-        for p in pages_freq_for_host[:5]:
-            res['top_10_hosts'][-1]['breakdown'].append((p, hosts[h]['pages'][p]))
-
-    res['error_in_lines'] = errors
+    res["perc_succ_reqs"] = f"{100 * succ/total:.2f}"
+    res["perc_fail_reqs"] = f"{100 * fails/total:.2f}"
+    # extract host-based stats
+    hosts_freq = sorted(hosts.keys(), key=lambda x: hosts[x]["count"], reverse=True)
+    # get config.top_hosts most active hosts
+    for h in hosts_freq[: config.top_hosts]:
+        res["top_hosts"].append(
+            {"host": h, "count": hosts[h]["count"], "breakdown": []}
+        )
+        pages_freq_for_host = sorted(
+            hosts[h]["pages"].keys(), key=lambda x: hosts[h]["pages"][x], reverse=True
+        )
+        # get top config.top_pages_per_host pages breakdown for host
+        for p in pages_freq_for_host[: config.top_pages_per_host]:
+            res["top_hosts"][-1]["breakdown"].append((p, hosts[h]["pages"][p]))
+    # errors
+    res["error_in_lines"] = errors
+    # all stats return as a dictionary
     return res
 
 
-def execute_script(input_args):
-    parsed_args = get_parser().parse_args(input_args)
-    print(parsed_args)
-    if parsed_args.test:
-        print("Testing the app.")
-    res = read_log()
-    print(json.dumps(res, indent=4))
-
-
 def main():
-    # Entry point to the app. Call in test method
-    execute_script(sys.argv[1:])
+    # read in config for this run
+    with open("config.json") as json_file:
+        config = Config(cfgdict=json.load(json_file))
+        res = stats(config=config)
+        print(json.dumps(res, indent=4))
 
 
 if __name__ == "__main__":
