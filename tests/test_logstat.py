5905128f0bb4fff0b5b91c7285246c2736af7333
V <12730010+zerogvt@users.noreply.github.com>
2022-01-18 11:39:48 +0200

    Update test_logstat.py

--- tests/test_logstat.py
+++ tests/test_logstat.py
@@ -1,5 +1,5 @@
 import unittest
-from logstat.logstat import stats, Config
+from logstat.logstat import Config, LogLine
 
 
 class TestLogStats(unittest.TestCase):
@@ -23,5 +23,43 @@ class TestLogStats(unittest.TestCase):
         self.assertEqual(UAT.top_pages_per_host, cfg['top_pages_per_host'])
         self.assertEqual(UAT.error_in_lines, cfg['error_in_lines'])
 
-    def test_one(self):
-        self.assertTrue(True)
+    def test_logline(self):
+        legit_line = 'uplherc.upl.com - - [01/Aug/1995:00:00:07 -0400] "GET / HTTP/1.0" 304 10'
+        lg = LogLine(legit_line)
+        print(lg.page)
+        self.assertEqual(lg.host, "uplherc.upl.com")
+        self.assertEqual(lg.time, "01/Aug/1995:00:00:07 -0400")
+        self.assertEqual(lg.page, "/")
+        self.assertEqual(lg.verb, "GET")
+        self.assertEqual(lg.version, "HTTP/1.0")
+        self.assertEqual(lg.status, 304)
+        self.assertEqual(lg.nbytes, 10)
+
+    def test_logline_missing_nbytes(self):
+        legit_line = 'uplherc.upl.com - - [01/Aug/1995:00:00:07 -0400] "GET / HTTP/1.0" 304 '
+        lg = LogLine(legit_line)
+        print(lg.page)
+        self.assertEqual(lg.host, "uplherc.upl.com")
+        self.assertEqual(lg.time, "01/Aug/1995:00:00:07 -0400")
+        self.assertEqual(lg.page, "/")
+        self.assertEqual(lg.verb, "GET")
+        self.assertEqual(lg.version, "HTTP/1.0")
+        self.assertEqual(lg.status, 304)
+        self.assertEqual(lg.nbytes, 0)
+
+    def test_logline_missing_version(self):
+        legit_line = 'uplherc.upl.com - - [01/Aug/1995:00:00:07 -0400] "GET / " 304 10'
+        lg = LogLine(legit_line)
+        print(lg.page)
+        self.assertEqual(lg.host, "uplherc.upl.com")
+        self.assertEqual(lg.time, "01/Aug/1995:00:00:07 -0400")
+        self.assertEqual(lg.page, "/")
+        self.assertEqual(lg.verb, "GET")
+        self.assertEqual(lg.status, 304)
+        self.assertEqual(lg.nbytes, 10)
+        self.assertEqual(lg.version, None)
+
+    def test_logline_malformed_raises(self):
+        bad_line= 'zooropa.res.cmu.edu - - [31/Aug/1995:10:36:28 -0400] "GET /htbin/cdt_clock.pl HTTP/1.0From:  <berend@blazemonger.pc.cc.cmu.edu>" 200 704'
+        with self.assertRaises(ValueError):
+            lg = LogLine(bad_line)
